78a6abcbc6590f0c939c3ebeb238fc3f46522b9b
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-08-06 00:49:16 -0700

    save the converted audio in text file

--- voice-to-text.py
+++ voice-to-text.py
@@ -5,22 +5,32 @@ r = sr.Recognizer()
 
 def record_text():
     # Loop in case of error
-    res = ""
+    full_text = ""
     while(True):
         try:
             # use michrophone as input
             with sr.Microphone() as source2:
+                # Prepare recognizer to receive input
                 r.adjust_for_ambient_noise(source2, duration=0.2)
+                # Listens for user input
                 audio2 = r.listen(source2)
-                text = r.recognize_google(audio2)
-                # Print the converted audio to text
-                res += text + " "
-                print(res)
+                # Use google to recognize the audio
+                text = r.recognize_google(audio2) + " "
+                if text == "stop ":
+                    print("\nListening Stoped.")
+                    return full_text
+                full_text += text
+
         except sr.RequestError as e:
             print("Could not request results. " + e)
         except sr.UnknownValueError as e:
             print("Unknown error")
 
+def create_text_file(text):
+    with open("recording.txt", "w") as f:
+        f.write(text)
+
 if __name__ == "__main__":
     print("Listening...")
-    record_text()
+    text = record_text()
+    create_text_file(text)
